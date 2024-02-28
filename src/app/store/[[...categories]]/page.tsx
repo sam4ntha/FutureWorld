e78778c927e5b03ad0ca65c9b6356b5b434df2ea
Samantha <stap0428@gmail.com>
2024-02-28 17:50:13 -0600

    1.7 Manejo de parámetros en rutas

---
+++ src/app/store/[[...categories]]/page.tsx
@@ -0,0 +1,15 @@
+interface CategoryProps {
+    params: {
+        categories: string[],
+        searchParams: string
+    }
+}
+
+export default function Category(props: CategoryProps){
+    console.log(props)
+    const { categories } = props.params
+    console.log(categories)
+    return(
+        <h1>Categoría Dinámica: {categories}</h1>
+    )
+}
