63547b477fa44bfaa9cb3d7e20547506c47ab09a
Samantha <stap0428@gmail.com>
2024-02-28 23:58:13 -0600

    08 2.1 CSS Modules en Next

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,7 +1,11 @@
+import styles from './Hero.module.css';
+
 export const Hero = () => {
+    console.log(styles)
     return(
-        <section>
-            <h1>Hero</h1>
+        <section className={styles.Hero}>
+            <h1>Future World</h1>
+            <h2>Empowering your tomorrow, today!</h2>
         </section>
     )
 }
