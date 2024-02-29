be551674573da8ad6e3d29eb10386158172303df
Samantha <stap0428@gmail.com>
2024-02-28 21:53:47 -0600

    1.8 React Server Components - use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('Hola Mundo pagina de inicio')
   return (
     <main className={styles.main}>
       <h1>Hola mundo</h1>
