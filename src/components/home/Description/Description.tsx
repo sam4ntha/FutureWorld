65132614b911a94675053f3bac3f4c25b35e3899
Samantha <stap0428@gmail.com>
2024-03-01 21:18:15 -0600

    11 2.4 Como agregar archivos estáticos

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,21 @@
+import styles from './Description.module.sass';
+import Image from 'next/image';
+
 export const Description = () => {
     return(
-        <section>
-            <h1>Descripción</h1>
+        <section className={styles.Description}>
+            <div className={styles.Description__imageContainer}>
+            <Image 
+                src="/images/description.jpeg" 
+                alt="Products marketplace" 
+                width={500} 
+                height={300} 
+            />
+            </div>
+            <div className={styles.Description__text}>
+                <h2>Bring the future today</h2>
+                <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+            </div>
         </section>
     )
 }
