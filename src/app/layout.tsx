3518b84635cfb55b4d7d215f364d218e52cc23ab
Samantha <stap0428@gmail.com>
2024-03-20 15:00:52 -0600

    19 3.5 Route grouping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { Roboto } from 'next/font/google';
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
+import { Hero } from "app/components/home/Hero";
 import "app/sass/globals.sass"
+import { Description } from 'app/components/home/Description';
 
 const roboto = Roboto({
   weight: ["100", "300", "500", "700"],
