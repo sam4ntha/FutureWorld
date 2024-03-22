22614554ebbf33d6982799bc9af4dedd945b8970
Samantha <stap0428@gmail.com>
2024-03-22 01:20:12 -0600

    25 4.7 Patrones data fetching

--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -3,5 +3,8 @@ import { env } from "app/config/env"
 export const shopifyUrls = {
     products: {
         'all':`${env.SHOPIFY_HOSTNAME}admin/api/2024-01/products.json`
+    }, 
+    collections: {
+        'all':`${env.SHOPIFY_HOSTNAME}admin/api/2024-01/smart_collections.json`
     }
 }
