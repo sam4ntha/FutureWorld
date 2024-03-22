21282c5bb4e03f97c4eef2bf57f39463df100ddc
Samantha <stap0428@gmail.com>
2024-03-21 20:02:41 -0600

    20 Manejo de errores

---
+++ src/app/(home)/error.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { useEffect } from "react";
+
+interface ErrorProps{
+    error: Error;
+    reset: () => void;
+}
+
+export default function Error({error, reset}: ErrorProps){
+    
+    useEffect(() => {
+        console.log(error)
+    }, [])
+
+    return(
+        <div style={{ padding: '10rem',}}>
+            <h1> :o </h1>
+            <p>Ha ocurrido un error</p>
+            <button onClick={reset}>Intentar de nuevo</button>
+        </div>
+    )
+}
